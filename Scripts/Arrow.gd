da406a1786ef2a56505536ad9fc0a309fbec4079
Jayanth L <jayanthl@protonmail.com>
2019-07-21 21:16:07 +0530

    direction correct

--- Scripts/Arrow.gd
+++ Scripts/Arrow.gd
@@ -3,16 +3,19 @@ extends KinematicBody
 var shoot = false
 signal fall_tree
 
+export var speed_div = 100
+
 func _ready():
 	get_tree().get_root().get_node("Main").connect("collided", self, "stop")
-	shoot = true
-	set_as_toplevel(true)
 	pass
 
 var position = 0.01
 func _process(delta):
+	if Input.is_mouse_button_pressed(1):
+		shoot = true
 	if shoot:
-		move_and_collide(Vector3(0, 0, -0.01))
+		set_as_toplevel(true)
+		move_and_collide(get_global_transform().basis.z.normalized()/ speed_div)
 
 func stop():
 	print("Got sisgnal")
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,14 +1,23 @@
 extends Spatial
 
 signal collided
+var instance
 
 var arrow = preload("res://ModelScenes/Arrow.tscn")
 func _ready():
-	arrow.connect("fall_tree", self, "fall_tree_fu")
-	$Camera/SpawnPosition.add_child(arrow.instance())
+	instance = arrow.instance()
+	instance.connect("fall_tree", self, "respawn_arrow")
+	$Camera/SpawnPosition.add_child(instance)
 
 
 func _on_PineTree_body_shape_entered(body_id, body, body_shape, local_shape):
 	print("Something collided:" + str(body.name))
-	emit_signal("collided")
-	$PineTree/AnimationPlayer.play("FallTree")
+	if("Arrow" in body.name):
+		emit_signal("collided")
+		body.queue_free()
+		$PineTree/AnimationPlayer.play("FallTree")
+
+func respawn_arrow():
+	instance = arrow.instance()
+	instance.connect("fall_tree", self, "respawn_arrow")
+	$Camera/SpawnPosition.add_child(instance)
